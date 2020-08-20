45f81dfd9a944ebc708e37f6982c861215871992
nixon2105 <radivilov1991@mail.ru>
2020-08-20 08:39:16 +0300

    feat: add function delete element list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,14 +7,30 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
 
+  deleteItem = (id) => {
+    this.setState(({todoData})=> {
+
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+
+      return {
+        todoData: newArray
+      }
+    })
+  }
+ 
+render() {
   return (
     <div className="todo-app">
       <AppHeader toDo={1} done={3} />
@@ -24,10 +40,12 @@ const App = () => {
       </div>
 
       <TodoList 
-        todos={todoData}
-        onDeleted = { (id) => console.log('del', id) } />
+        todos={this.state.todoData}
+        onDeleted = {this.deleteItem } />
     </div>
   );
 };
+};
 
-export default App;
+  
+
